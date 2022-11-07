4460528ed1dbbfc69c2f3f644446fd53e8eb8c87
KaurSup <114486962+KaurSup@users.noreply.github.com>
2022-11-07 16:23:15 +0530

    Update sample-bash.sh

--- sample-bash.sh
+++ sample-bash.sh
@@ -1,6 +1,14 @@
 echo 1. Whats in this directory ?
 ls -a
 
-echo "This script is about to run another script."
-sh ./unit_tests/run_L1_full_test.sh
-echo "This script has just run another script."
+
+echo 2. Whats the Python version ?
+python --version
+
+echo 3. Whats the Ubuntu version ?
+lsb_release -a
+
+echo "Thats all the info I have folks"
+#echo "This script is about to run another script."
+#sh ./unit_tests/run_L1_full_test.sh
+#echo "This script has just run another script."
