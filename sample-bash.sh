f3cb4da6628ddc916512dcaa2c8586f6045662aa
KaurSup <114486962+KaurSup@users.noreply.github.com>
2022-11-07 16:28:19 +0530

    Update sample-bash.sh

--- sample-bash.sh
+++ sample-bash.sh
@@ -6,7 +6,7 @@ echo 2. Whats the Python version ?
 python --version
 
 echo 3. Whats the Ubuntu version ?
-lsb_release -a
+/etc/os-release
 
 echo "Thats all the info I have folks"
 #echo "This script is about to run another script."
