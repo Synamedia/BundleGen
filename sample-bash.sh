3f5752b2b3b36d26ceab8a67f6d32fff41f6ce97
KaurSup <114486962+KaurSup@users.noreply.github.com>
2022-11-07 16:49:42 +0530

    Update sample-bash.sh

--- sample-bash.sh
+++ sample-bash.sh
@@ -1,12 +1,14 @@
 echo 1. Whats in this directory ?
 ls -a
 
-
 echo 2. Whats the Python version ?
 python --version
 
 echo 3. Whats the Ubuntu version ?
 hostnamectl
+
+echo 4. Whats the pip version?
+pip --version
 
 echo "Thats all the info I have folks"
 #echo "This script is about to run another script."
