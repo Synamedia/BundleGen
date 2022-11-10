076950d44ade8fffff7f078a06bfd8a966d60849
KaurSup <114486962+KaurSup@users.noreply.github.com>
2022-11-10 13:38:24 +0530

    Update DAC-SDK-checkout.sh

--- DAC-SDK-checkout.sh
+++ DAC-SDK-checkout.sh
@@ -1,4 +1,4 @@
 mkdir DAC-SDK
-cd DAK-SDK
+bash cd DAK-SDK
 git clone https://android.googlesource.com/tools/repo
 ./repo/repo init -u https://github.com/stagingrdkm/lgpub/ -m manifests/dac-dunfell-3.1.6-manifest.xml
