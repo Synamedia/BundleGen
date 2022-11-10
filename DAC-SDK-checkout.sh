20ed741752f7751859cddf82d59d5b68d45e6a97
KaurSup <114486962+KaurSup@users.noreply.github.com>
2022-11-10 15:29:50 +0530

    Update DAC-SDK-checkout.sh

--- DAC-SDK-checkout.sh
+++ DAC-SDK-checkout.sh
@@ -7,6 +7,10 @@ pwd
 echo Whats in this directory ?
 ls -al
 
+echo Whats in the CD directory?
+ls -al /home/runner/work/BundleGen/BundleGen/DAK-SDK
+
+
 cd /home/runner/work/BundleGen/BundleGen/DAK-SDK
 git clone https://android.googlesource.com/tools/repo
 ./repo/repo init -u https://github.com/stagingrdkm/lgpub/ -m manifests/dac-dunfell-3.1.6-manifest.xml
